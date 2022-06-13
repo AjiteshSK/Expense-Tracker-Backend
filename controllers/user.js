213532eabed34232a1e32a7e2e41fcd0498bd614
ajiteshk <ajiteshkahlon51@gmail.com>
2022-06-13 20:15:21 +0530

    SignUp

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,30 @@
 import db from "../db/database.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const userController = {
   signUp: async (req, res) => {
     try {
       const { username, email, password } = req.body;
 
-      //#region Check for existing email
-      const result = db.get(
-        `SELECT email FROM users WHERE email = "${email}"`,
-        [],
-        (err, rows) => {
-          if (err) {
-            console.log("Error in db.GET", err);
-          }
-          if (rows) {
-            console.log("ROWS", rows);
-          }
-        }
+      const existingEmail = await db.get(
+        `SELECT email FROM users WHERE email = "${email}"`
       );
-      console.log("RESULT", result);
-      //#endregion
+
+      if (existingEmail) {
+        return res.json(409).json({ message: "This E-mail is already in use" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const id = 676;
+
+      const ress = await db.run(
+        "INSERT INTO Users (id, username, email, password) VALUES (?, ?, ?, ?)",
+        [id, username, email, hashedPassword]
+      );
+
+      console.log("RESS", ress);
     } catch (err) {
       console.error("Error in signUp controller", err);
     }
--- index.js
+++ index.js
@@ -2,6 +2,5 @@ import app from "./app.js";
 import db from "./db/database.js";
 
 app.listen(8080, async () => {
-  const rows = await db.all("SELECT * FROM Users");
-  console.log("ROWS", rows);
+  console.log("App listening on port 8080");
 });
